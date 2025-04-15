6d1f93d6de267b115fe0b7927762809d6875a057
diegoliv-scaler <diego@scalermarketing.com>
2025-04-15 15:05:44 -0300

    added extra logic for loading states

--- src/globals.js
+++ src/globals.js
@@ -8,6 +8,25 @@ import { setPathAnimations } from "./modules/setPathAnimation";
 import { initFormSubmit } from "./modules/formSubmit";
 import { setVideosModal } from "./modules/videoModal";
 
+window.addEventListener("beforeunload", () => {
+  console.log("beforeunload");
+  setLoadingStates();
+});
+
+window.addEventListener("popstate", () => {
+  console.log("popstate");
+  setLoadingStates();
+});
+
+document.addEventListener("pageshow", (event) => {
+  console.log("pageshow", event);
+  // Check if the page was restored from the bfcache
+  if (event.persisted) {
+    // Re-run your loading animation
+    setLoadingStates();
+  }
+});
+
 // wait until DOM is ready (html and svg markup)
 document.addEventListener("DOMContentLoaded", function () {
   initMenu();
@@ -16,10 +35,10 @@ document.addEventListener("DOMContentLoaded", function () {
   setPathAnimations();
   initFormSubmit();
   setVideosModal();
+  setLoadingStates();
 });
 
 document.fonts.ready.then(() => {
   setStaggerText();
   setRevealHeading();
-  setLoadingStates();
 });
